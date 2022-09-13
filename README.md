8d35377e1d69530f0a64bc19fd800de65c400074
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-09-14 00:33:39 +0200

    fsda

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ rpk topic create filebeat \
 ```
 
 ```bash
-kubectl  get pods  | grep -i producer | awk '{print $1}' | xargs
-kubectl delete pods --force `kubectl  get pods  | grep -i producer | awk '{print $1}' | xargs`
+for i in `seq 1 100` ; do
+  kubectl delete pods --force `kubectl  get pods  | grep -i producer | awk '{print $1}' | head -10 | xargs`
+done
 ```
--- producer/deploy.sh
+++ producer/deploy.sh
@@ -4,7 +4,7 @@ for i in `seq 0 4` ; do
     helm upgrade --install "producer${i}" . \
         -f values.yaml \
         --set "producer.fullnameOverride=producer${i}" \
-        --set "producer.replicas=100" \
+        --set "producer.replicas=50" \
         --set "producer.extraVolumes[0].name=snapshot" \
         --set "producer.extraVolumes[0].persistentVolumeClaim.claimName=snapshot${i}"
 done
