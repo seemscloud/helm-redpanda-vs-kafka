285c5e98c49c6b0137b0ef455f80a9caef88df3c
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-09-18 17:18:17 +0200

    sdffdsa

--- redpanda-operator/deploy.sh
+++ redpanda-operator/deploy.sh
@@ -3,6 +3,3 @@ kubectl apply -k https://github.com/redpanda-data/redpanda/src/go/k8s/config/crd
 
 helm dependency update
 helm upgrade --install redpanda . -f values.yaml
-echo "sleep 10 seconds for operator waiting for operator"
-sleep 10
-kubectl apply -f redpanda.yaml
